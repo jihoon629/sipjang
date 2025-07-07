be7a250a8c1cccae24fe04bd92c5b0c57b92d2b6
jihoon629 <jihonn629@gmail.com>
2025-07-07 14:29:54 +0900

    직종 이력서 수정

--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -43,6 +43,29 @@ function Resume() {
   }, [user, resumeData.name, nameInitialized]);
 
 
+  // 직종 목록 분리
+const GENERAL_WORKERS = [
+  // 일반 직종
+  "보통인부", "자재정리", "신호수", "준공청소", "해체정리", "작업팀장", "세대청소", 
+  "곰방", "양중", "안전관리", "안전시설", "화재감시자", "안전감시단", "농촌", 
+  "경계석공", "토류판공", "보양공", "전기공", "알폼", "경비원", "할석공", 
+  "직영-건축반장", "직영-안전반장", "미화", "고정 신호수",
+];
+
+const SKILLED_WORKERS = [
+  // 기능공 직종
+  "건축배관", "형틀목공", "강구조", "건축목공", "철근", "비계", "조경", "석공", 
+  "도장", "미장", "토공", "조적", "타일", "일반용접", "콘크리트", "수장", "방수", 
+  "덕트", "창호", "도배", "건축기계설비", "철거", "건출", "일반기계설비", 
+  "패널조립", "보온", "유리", "플랜트기계설비", "제관", "플랜트계측설비", 
+  "코킹", "포장", "벌목", "궤도", "상하수도배관", "보링", "발파", "지붕", 
+  "플랜트배관", "잠수", "플랜트제관", "플랜트용접", "준설", "플랜트전기설비", 
+  "플랜트보온", "보일러", "일반특수용접", "플랜트덕트", "플랜트특수용접",
+];
+
+// 기타 추가
+const OTHER_JOB_TYPES = ["기타"];
+
   // 기술 선택 옵션들
   const skillOptions = [
     "용접", "미장", "타일", "도장", "철근", "목공", "전기", "배관", 
@@ -155,6 +178,7 @@ function Resume() {
   
   // 주소 팝업 상태
   const [showAddressPopup, setShowAddressPopup] = useState(false);
+  const [showJobTypeDropdown, setShowJobTypeDropdown] = useState(false);
   
   // 이미지 모달 상태
   const [showImageModal, setShowImageModal] = useState(false);
@@ -164,6 +188,14 @@ function Resume() {
   const handleResumeChange = (e) => {
     const { name, value } = e.target;
     setResumeData({ ...resumeData, [name]: value });
+  };
+
+  const handleJobTypeSelect = (jobType) => {
+    setResumeData(prev => ({
+        ...prev,
+        jobType: jobType
+    }));
+    setShowJobTypeDropdown(false);
   };
 
   // 기술 추가/제거 핸들러
@@ -510,18 +542,61 @@ function Resume() {
           <div className="resume-form-item">
             <label className="resume-form-label">희망 직종</label>
             {editMode ? (
-              <select 
-                className="resume-form-select"
-                name="jobType"
-                value={resumeData.jobType}
-                onChange={handleResumeChange}
-              >
-                <option value="건설">건설</option>
-                <option value="운송">운송</option>
-                <option value="제조">제조</option>
-                <option value="서비스">서비스</option>
-                <option value="기타">기타</option>
-              </select>
+              <div className="custom-select-container">
+                <input 
+                    name="jobType" 
+                    value={resumeData.jobType} 
+                    onChange={handleResumeChange}
+                    onClick={() => setShowJobTypeDropdown(!showJobTypeDropdown)}
+                    placeholder="직종을 선택하세요"
+                    readOnly
+                />
+                {showJobTypeDropdown && (
+                    <div className="custom-select-options">
+                        <div className="job-type-categories">
+                            <div className="job-type-category">
+                                <h4 className="category-title">일반인부</h4>
+                                <div className="job-type-list">
+                                    {GENERAL_WORKERS.map((jobType) => (
+                                        <div 
+                                            key={jobType} 
+                                            className="job-type-option"
+                                            onClick={() => handleJobTypeSelect(jobType)}
+                                        >
+                                            {jobType}
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                            <div className="job-type-category">
+                                <h4 className="category-title">기능공</h4>
+                                <div className="job-type-list">
+                                    {SKILLED_WORKERS.map((jobType) => (
+                                        <div 
+                                            key={jobType} 
+                                            className="job-type-option"
+                                            onClick={() => handleJobTypeSelect(jobType)}
+                                        >
+                                            {jobType}
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="job-type-other">
+                            {OTHER_JOB_TYPES.map((jobType) => (
+                                <div 
+                                    key={jobType} 
+                                    className="job-type-option other"
+                                    onClick={() => handleJobTypeSelect(jobType)}
+                                >
+                                    {jobType}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                )}
+              </div>
             ) : (
               <div className="resume-form-value">{resumeData.jobType}</div>
             )}
